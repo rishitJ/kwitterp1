41e4ea5e8062d4238bd88b13c7f2342295bc2791
rishitJ <121947502+rishitJ@users.noreply.github.com>
2023-07-01 14:01:20 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,8 @@
+function add_user()
+{
+    user_name=document.getElementById("user_name").value
+    localStorage.setItem("user_name", user_name)
+    
+    window.location="kwitter_room.html"
+    return false
+}
